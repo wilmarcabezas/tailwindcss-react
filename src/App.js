762dc4f5889da6a39ddae122cfa2111be37909c3
wilmarux <wilmar.cabezas@gmail.com>
2022-03-21 22:23:18 -0500

    ÂRelease

--- src/App.js
+++ src/App.js
@@ -24,9 +24,11 @@ function App() {
 
       {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
 
-      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="flex flex-wrap">
         {images.map(image => (
+        <div className='mx-auto mt-2'>
           <ImageCard key={image.id} image={image} />
+          </div>
         ))}
       </div>}
     </div>
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,39 +1,52 @@
-import React from 'react';
+import React from "react";
 
 const ImageCard = ({ image }) => {
-  const tags = image.tags.split(',');
-
+  const tags = image.tags.split(",");
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src={image.webformatURL} alt="" className="w-full"/>
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by {image.user}
+      
+      <div className="flex">
+        <div className="px-6 py-4 ms:w-full md:w-1/2">
+          <img
+            src={image.webformatURL}
+            alt=""
+            className="  w-full rounded "
+          />
         </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            {image.views}
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            {image.downloads}
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            {image.likes}
-          </li>
-        </ul>
+
+        <div className="px-6 py-4 rounded ms:w-full md:w-1/2">
+          <div className="font-bold text-orange-700 text-xl mb-2">
+            Photo by {image.user}
+          </div>
+          <ul>
+            <li>
+              <strong>Views: </strong>
+              {image.views}
+            </li>
+            <li>
+              <strong>Downloads: </strong>
+              {image.downloads}
+            </li>
+            <li>
+              <strong>Likes: </strong>
+              {image.likes}
+            </li>
+          </ul>
+        </div>
       </div>
+
       <div className="px-6 py-4">
         {tags.map((tag, index) => (
-          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #{tag}
-        </span>
+          <span
+            key={index}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+          >
+            #{tag}
+          </span>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ImageCard;
